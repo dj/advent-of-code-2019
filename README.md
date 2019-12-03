3e4957926b695f6e08df0d088fb775399fcaee7a
Daniel Hartman <dhartman1@wayfair.com>
2019-12-03 01:54:09 -0500

    Refactor day 1, add readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Advent of Code 2019
+
+Usage:
+```
+cd day-x
+dotnet restore
+dotnet run
+```
+
--- day-1/Program.cs
+++ day-1/Program.cs
@@ -5,36 +5,40 @@ namespace day_1
 {
   class Program
   {
-    static double Part1(string[] modules)
+    static double Part1(double[] modules)
     {
-        double totalFuel = 0;
-        foreach (string module in modules)
-        {
-            double weight = Double.Parse(module);
-            double fuel = Math.Floor(weight / 3) - 2;
-            totalFuel += fuel;
-        }
-        return totalFuel;
+      double totalFuel = 0;
+      foreach (double weight in modules)
+      {
+        double fuel = Math.Floor(weight / 3) - 2;
+        totalFuel += fuel;
+      }
+      return totalFuel;
     }
 
-    static double Part2(string[] modules)
+    static double Part2(double[] modules)
     {
-        double totalFuel = 0;
-        foreach (string module in modules)
+      double totalFuel = 0;
+      foreach (double weight in modules)
+      {
+        double fuel = Math.Floor(weight / 3) - 2;
+        while (fuel > 0)
         {
-            double weight = Double.Parse(module);
-            double fuel = Math.Floor(weight / 3) - 2;
-            while (fuel > 0) {
-                totalFuel += fuel;
-                fuel = Math.Floor(fuel / 3) - 2;
-            }
+          totalFuel += fuel;
+          fuel = Math.Floor(fuel / 3) - 2;
         }
-        return totalFuel;
+      }
+      return totalFuel;
     }
 
     static void Main(string[] args)
     {
-      string[] modules = File.ReadAllLines("day-1.txt");
+      string[] lines = File.ReadAllLines("day-1.txt");
+      double[] modules = new double[lines.Length];
+      for (int i = 0; i < lines.Length; i++)
+      {
+        modules[i] = Double.Parse(lines[i]);
+      }
 
       Console.WriteLine("Part 1: " + Part1(modules));
       Console.WriteLine("Part 2: " + Part2(modules));
